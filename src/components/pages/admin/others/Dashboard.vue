5e46f4b86e44c2e73e7f6438dcdf61a399bccb20
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2023-03-31 12:45:04 -0400

    dashboard function without details of delay

--- src/components/pages/admin/others/Dashboard.vue
+++ src/components/pages/admin/others/Dashboard.vue
@@ -6,21 +6,94 @@ import jwt_decode from 'jwt-decode';
 
 
 let user = [];
-let token = "";
-let urgentList = []
-
-
+let urgentList = [];
+let listDG = [];
+let listDT = [];
+let listGOP = [];
+let listIOP = [];
+let listT = [];
+let listE = [];
+let listC = [];
+let listCC = [];
+let listF = [];
+let listD = [];
+let emptyPage = true;
 
 //Exports
 export default {
    data() {
       return {
          user,
-         urgentList
+         urgentList,
+         listDG,
+         listDT,
+         listGOP,
+         listIOP,
+         listT,
+         listE,
+         listC,
+         listCC,
+         listF,
+         listD,
+         emptyPage
       }
    },
    methods: {
       
+      //Metodo que verifica si las listas de departamentos estan iniciadas
+      verifyLists(){
+            if(this.listDG.length === 0)
+               this.listDG = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listDT.length === 0)
+               this.listDT = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listGOP.length === 0)
+               this.listGOP = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listIOP.length === 0)
+               this.listIOP = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listT.length === 0)
+               this.listT = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listE.length === 0)
+               this.listE = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listC.length === 0)
+               this.listC = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listCC.length === 0)
+               this.listCC = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listF.length === 0)
+               this.listF = null;
+            else
+               this.emptyPage = false;
+
+            if(this.listD.length === 0)
+               this.listD = null;
+            else
+               this.emptyPage = false;
+            
+      },
+
       //Metodo que obtiene de cookies el usuario que inicio sesion
       getUserFromCookies(){
          return Cookies.get("userLogged");
@@ -51,8 +124,8 @@ export default {
             alert("Error: "+error.response.data.message);
          });
 
-},
-
+      },
+      
    },
    props: [
 
@@ -78,6 +151,17 @@ export default {
             )
             .then((res) => {
                this.urgentList = res.data.data;
+               this.listDG = this.urgentList.DG;
+               this.listDT = this.urgentList.DT;
+               this.listGOP = this.urgentList.GOP;
+               this.listIOP = this.urgentList.IOP;
+               this.listT = this.urgentList.T;
+               this.listE = this.urgentList.E;
+               this.listC = this.urgentList.C;
+               this.listCC = this.urgentList.CC;
+               this.listF = this.urgentList.F;
+               this.listD = this.urgentList.D;
+               this.verifyLists();
             })
             .catch((error) => {
                console.log(error.message);
@@ -94,7 +178,7 @@ export default {
       <div class="container-fluid">
          <div class="row mb-2">
             <div class="col-sm-6">
-               <h1 class="m-0">Tablero</h1>
+               <h1 class="m-0 font-weight-bold">Tablero</h1>
             </div>
             <div class="col-sm-6">
                <ol class="breadcrumb float-sm-right">
@@ -106,17 +190,206 @@ export default {
       </div>
    </div>
 
-   <div class="container-dashboard"> 
-      <div class="card card-primary card-outline" v-for="item in urgentList" :key="item.id">
-         <div class="card-header">
-            <h5 class="m-0">{{item.department}}</h5>
+   <div class="container-dashboard">
+
+         <!--DISEÑO GRAFICO-->
+         <div class="card card-primary card-outline" v-if="listDG">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Diseño Gráfico</h3>
+            </div>
+            <div class="card-body">
+               <h5>Pedidos con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listDG" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.request}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
          </div>
-         <div class="card-body">
-            <p class="card-text">{{item.request}}</p>
-            <a class="btn btn-primary">Revisar</a>
+
+         <!--Diseño Textil-->
+         <div class="card card-primary card-outline" v-if="listDT">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Diseño Textil</h3>
+            </div>
+            <div class="card-body">
+               <h5>Pedidos con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listDT" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.request}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
          </div>
-      </div>
+
+         <!--Generar OP-->
+         <div class="card card-primary card-outline" v-if="listGOP">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Generar OP</h3>
+            </div>
+            <div class="card-body">
+               <h5>Pedidos con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listGOP" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.request}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Imprimir OP-->
+         <div class="card card-primary card-outline" v-if="listIOP">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Imprimir OP</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listIOP" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Tejeduría-->
+         <div class="card card-primary card-outline" v-if="listT">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Tejeduría</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listT" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Enrollado-->
+         <div class="card card-primary card-outline" v-if="listE">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Enrollado</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listE" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+         
+         <!--Corte-->
+         <div class="card card-primary card-outline" v-if="listC">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Corte</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listC" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Control de Calidad-->
+         <div class="card card-primary card-outline" v-if="listCC">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Control de Calidad</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listCC" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Facturación-->
+         <div class="card card-primary card-outline" v-if="listF">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Facturación</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listF" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <!--Despacho-->
+         <div class="card card-primary card-outline" v-if="listD">
+            <div class="card-header">
+               <h3 class="m-0 font-weight-bold">Despacho</h3>
+            </div>
+            <div class="card-body">
+               <h5>Ordenes con retrasos:</h5>
+               <br/>
+               <ol>
+                  <li v-for="item in listD" :key="item.id">
+                     <p class="card-text font-weight-bold">{{item.order}} </p>
+                     <p>
+                        Retraso de {{ item.delay }} días <a class="btn btn-outline-danger btn-sm">revisar</a>
+                     </p>
+                  </li>
+               </ol>
+            </div>
+         </div>
+
+         <div v-if="emptyPage">
+            <br/>
+            <h5 class="font-weight-light">No hay ningun pedido u orden con retraso.</h5>
+         </div>
+  
    </div>
+
+
    
 </template>
 
@@ -130,4 +403,5 @@ export default {
    padding-right: 20px;
    justify-content: space-evenly;
  }
+
 </style>
