7c32cbdf48c59efc4d39b4696518cb5d498b72bd
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2023-05-04 15:20:33 -0400

    Update Time_Line.vue

--- src/components/pages/admin/others/Time_Line.vue
+++ src/components/pages/admin/others/Time_Line.vue
@@ -162,10 +162,10 @@ export default {
         },
 
         //Llenamos la lista de colores para los departamentos
-        async fillColorList(){
-            
+        async fillColorList() {
+
             //Diseño Gráfico
-            this.colorList.push("--accent-color:#41516C" )
+            this.colorList.push("--accent-color:#41516C")
 
             //Diseño Textil
             this.colorList.push("--accent-color:#FBCA3E")
@@ -198,9 +198,10 @@ export default {
 
         //Llenamos la linea de tiempo del pedido seleccionado
         async fillTimeLineProcess(process, requestName) {
-           
+
+            this.refresToken();
             this.processTimeLine = [];
-            
+
             await axios
                 .get("http://localhost:3000/process/get/for-time-line/" + process.id,
                     { headers: { Authorization: `Bearer ${this.getUserFromCookies()}` } }
@@ -209,11 +210,13 @@ export default {
 
                     this.requestName = requestName;
 
-                    for(let i = 0; i < res.data.data.length ; i++){
+                    for (let i = 0; i < res.data.data.length; i++) {
                         this.processTimeLine.push({ info: res.data.data[i], color: this.colorList[i] })
                     }
 
-                    this.fixDates();
+                    this.fixDatesAndBugs();
+
+                    this.processTimeLine.sort((a, b) => a.info.department.process_turn - b.info.department.process_turn )
 
                 })
                 .catch((error) => {
@@ -223,20 +226,25 @@ export default {
         },
 
         //Arreglamos las fechas para que se vean bien en la linea de tiempo
-        fixDates(){
+        fixDatesAndBugs() {
 
 
-            for(let i = 0; i < this.processTimeLine.length; i++){
-                if(this.processTimeLine[i].info.date_in){
+            for (let i = 0; i < this.processTimeLine.length; i++) {
+
+                if (this.processTimeLine[i].info.date_in) {
                     this.processTimeLine[i].info.date_in = new Date(this.processTimeLine[i].info.date_in).toLocaleString();
-                }else{
-                    this.processTimeLine[i].info.date_in = "No ha entrado al departamento.";                
+                } else {
+                    this.processTimeLine[i].info.date_in = "No ha entrado al departamento.";
                 }
 
-                if(this.processTimeLine[i].info.date_out){
+                if (this.processTimeLine[i].info.date_out) {
                     this.processTimeLine[i].info.date_out = new Date(this.processTimeLine[i].info.date_out).toLocaleString();
-                }else{
+                } else {
                     this.processTimeLine[i].info.date_out = "No ha salido del departamento."
+                }
+
+                if(!this.processTimeLine[i].info.operator){
+                    this.processTimeLine[i].info.operator = {name: "N", last_name: "/ A"}
                 }
 
             }
@@ -294,20 +302,20 @@ export default {
     </div>
 
     <!--AQUI EMPIEZA LA LINEA DE TIEMPO-->
-    <br/>
-    <h1 class="center-text font-weight-bold">Pedido: {{requestName}}</h1>
-    <br/>
+    <br />
+    <h1 class="center-text font-weight-bold">Pedido: {{ requestName }}</h1>
+    <br />
     <ul class="ul-time-line">
         <li v-for="process in processTimeLine" :key="process.id" :style="process.color">
-            <div class="date">{{process.info.department.name}}</div>
-            <br/>
-            <div class="title">Fecha de ingreso: {{process.info.date_in }}</div>
-            <div class="title">Fecha de salida: {{process.info.date_out}}</div>
-            <div class="descr">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat
-                est numquam harum, accusamus suscipit consequatur laboriosam!</div>
+            <div class="date">{{ process.info.department.process_turn + "." + process.info.department.name }}</div>
+            <br />
+            <div class="title">Fecha de ingreso: {{ process.info.date_in }}</div>
+            <div class="title">Fecha de salida: {{ process.info.date_out }}</div>
+            <div class="descr">
+                Este pedido fue suepervisado por: {{ process.info.operator.name + " " + process.info.operator.last_name }} 
+            </div>
         </li>
     </ul>
-
 </template>
 
 <style>
@@ -316,155 +324,161 @@ export default {
 *,
 *::before,
 *::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
 
 .ul-time-line {
-  --col-gap: 2rem;
-  --row-gap: 2rem;
-  --line-w: 0.25rem;
-  display: grid;
-  grid-template-columns: var(--line-w) 1fr;
-  grid-auto-columns: max-content;
-  column-gap: var(--col-gap);
-  list-style: none;
-  width: min(60rem, 90%);
-  margin-inline: auto;
+    --col-gap: 2rem;
+    --row-gap: 2rem;
+    --line-w: 0.25rem;
+    display: grid;
+    grid-template-columns: var(--line-w) 1fr;
+    grid-auto-columns: max-content;
+    column-gap: var(--col-gap);
+    list-style: none;
+    width: min(60rem, 90%);
+    margin-inline: auto;
 }
 
 /* line */
 .ul-time-line::before {
-  content: "";
-  grid-column: 1;
-  grid-row: 1 / span 20;
-  background: rgb(225, 225, 225);
-  border-radius: calc(var(--line-w) / 2);
+    content: "";
+    grid-column: 1;
+    grid-row: 1 / span 20;
+    background: rgb(225, 225, 225);
+    border-radius: calc(var(--line-w) / 2);
 }
 
 /* columns*/
 
 /* row gaps */
 .ul-time-line li:not(:last-child) {
-  margin-bottom: var(--row-gap);
+    margin-bottom: var(--row-gap);
 }
 
 /* card */
 .ul-time-line li {
-  grid-column: 2;
-  --inlineP: 1.5rem;
-  margin-inline: var(--inlineP);
-  grid-row: span 2;
-  display: grid;
-  grid-template-rows: min-content min-content min-content;
+    grid-column: 2;
+    --inlineP: 1.5rem;
+    margin-inline: var(--inlineP);
+    grid-row: span 2;
+    display: grid;
+    grid-template-rows: min-content min-content min-content;
 }
 
 /* date */
 .ul-time-line li .date {
-  --dateH: 3rem;
-  height: var(--dateH);
-  margin-inline: calc(var(--inlineP) * -1);
+    --dateH: 3rem;
+    height: var(--dateH);
+    margin-inline: calc(var(--inlineP) * -1);
 
-  text-align: center;
-  background-color: var(--accent-color);
+    text-align: center;
+    background-color: var(--accent-color);
 
-  color: white;
-  font-size: 1.25rem;
-  font-weight: 700;
+    color: white;
+    font-size: 1.25rem;
+    font-weight: 700;
 
-  display: grid;
-  place-content: center;
-  position: relative;
+    display: grid;
+    place-content: center;
+    position: relative;
 
-  border-radius: calc(var(--dateH) / 2) 0 0 calc(var(--dateH) / 2);
+    border-radius: calc(var(--dateH) / 2) 0 0 calc(var(--dateH) / 2);
 }
 
 /* date flap */
 .ul-time-line li .date::before {
-  content: "";
-  width: var(--inlineP);
-  aspect-ratio: 1;
-  background: var(--accent-color);
-  background-image: linear-gradient(rgba(0, 0, 0, 0.2) 100%, transparent);
-  position: absolute;
-  top: 100%;
+    content: "";
+    width: var(--inlineP);
+    aspect-ratio: 1;
+    background: var(--accent-color);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2) 100%, transparent);
+    position: absolute;
+    top: 100%;
 
-  clip-path: polygon(0 0, 100% 0, 0 100%);
-  right: 0;
+    clip-path: polygon(0 0, 100% 0, 0 100%);
+    right: 0;
 }
 
 /* circle */
 .ul-time-line li .date::after {
-  content: "";
-  position: absolute;
-  width: 2rem;
-  aspect-ratio: 1;
-  background: var(--bgColor);
-  border: 0.3rem solid var(--accent-color);
-  border-radius: 50%;
-  top: 50%;
+    content: "";
+    position: absolute;
+    width: 2rem;
+    aspect-ratio: 1;
+    background: var(--bgColor);
+    border: 0.3rem solid var(--accent-color);
+    border-radius: 50%;
+    top: 50%;
 
-  transform: translate(50%, -50%);
-  right: calc(100% + var(--col-gap) + var(--line-w) / 2);
+    transform: translate(50%, -50%);
+    right: calc(100% + var(--col-gap) + var(--line-w) / 2);
 }
 
 /* title descr */
 .ul-time-line li .title,
 .ul-time-line li .descr {
-  background: var(--bgColor);
-  position: relative;
-  padding-inline: 1.5rem;
+    background: var(--bgColor);
+    position: relative;
+    padding-inline: 1.5rem;
 }
+
 .ul-time-line li .title {
-  overflow: hidden;
-  font-weight: 500;
+    overflow: hidden;
+    font-weight: 500;
 }
+
 .ul-time-line li .descr {
-  padding-block-end: 1.5rem;
-  font-weight: 300;
+    padding-block-end: 1.5rem;
+    font-weight: 300;
 }
 
 @media (min-width: 40rem) {
     .ul-time-line {
-    grid-template-columns: 1fr var(--line-w) 1fr;
-  }
-  .ul-time-line::before {
-    grid-column: 2;
-  }
-  .ul-time-line li:nth-child(odd) {
-    grid-column: 1;
-  }
-  .ul-time-line li:nth-child(even) {
-    grid-column: 3;
-  }
+        grid-template-columns: 1fr var(--line-w) 1fr;
+    }
 
-  /* start second card */
-  .ul-time-line li:nth-child(2) {
-    grid-row: 2/4;
-  }
+    .ul-time-line::before {
+        grid-column: 2;
+    }
 
-  .ul-time-line li:nth-child(odd) .date::before {
-    clip-path: polygon(0 0, 100% 0, 100% 100%);
-    left: 0;
-  }
+    .ul-time-line li:nth-child(odd) {
+        grid-column: 1;
+    }
 
-  .ul-time-line li:nth-child(odd) .date::after {
-    transform: translate(-50%, -50%);
-    left: calc(100% + var(--col-gap) + var(--line-w) / 2);
-  }
-  .ul-time-line li:nth-child(odd) .date {
-    border-radius: 0 calc(var(--dateH) / 2) calc(var(--dateH) / 2) 0;
-  }
+    .ul-time-line li:nth-child(even) {
+        grid-column: 3;
+    }
+
+    /* start second card */
+    .ul-time-line li:nth-child(2) {
+        grid-row: 2/4;
+    }
+
+    .ul-time-line li:nth-child(odd) .date::before {
+        clip-path: polygon(0 0, 100% 0, 100% 100%);
+        left: 0;
+    }
+
+    .ul-time-line li:nth-child(odd) .date::after {
+        transform: translate(-50%, -50%);
+        left: calc(100% + var(--col-gap) + var(--line-w) / 2);
+    }
+
+    .ul-time-line li:nth-child(odd) .date {
+        border-radius: 0 calc(var(--dateH) / 2) calc(var(--dateH) / 2) 0;
+    }
 }
 
 .credits {
-  margin-top: 1rem;
-  text-align: right;
-}
-.credits a {
-  color: var(--color);
+    margin-top: 1rem;
+    text-align: right;
 }
 
+.credits a {
+    color: var(--color);
+}
 </style>
